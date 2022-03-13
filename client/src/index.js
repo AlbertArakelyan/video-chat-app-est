27454f0a4cb5df46758d5b45ca87a5ecf5f3673c
Albert Arakelyan <albertarakelyan1998@gmail.com>
2022-03-14 00:23:56 +0400

    deploy to heroku

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import { ContextProvider } from './contexts/SocketContext';
+
+import './styles.css';
+
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById('root'),
+);
